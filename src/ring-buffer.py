631fd2cf436a3639cbfc002dde5b6e448cb911da
Sri Hari Vignesh <sriharivignesh@outlook.com>
2018-03-15 22:54:24 -0700

    Add RingBuffer class

--- src/ring-buffer.py
+++ src/ring-buffer.py
@@ -2,18 +2,81 @@
 
 class RingBuffer(object):
     def __init__(self, size):
-        pass
+        '''
+        Initialise the buffer
+        '''
+        self.head = 0
+        self.tail = 0
+        self.max_size = size
+        self.buffer = [0 for i in xrange(0, size)]
+        self.full = False
+        self.empty = True
 
-    def put(self, element):
-        pass
+    def push(self, element):
+        '''
+        Push an element into the buffer
+        '''
+        if (self.is_full()):
+            raise RuntimeError('Buffer is full!')
 
-    def get(self):
-        pass
+        self.buffer[self.head] = element
+        self.head = (self.head + 1) % self.max_size
 
-    def isEmpty(self):
-        pass
+        self.empty = False
+        if self.head == self.tail:
+            self.full = True
+
+    def pop(self):
+        '''
+        Pop an element from the buffer
+        '''
+        if(self.is_empty()):
+            raise RuntimeError('Buffer is empty!')
+       
+        element = self.buffer[self.tail]
+
+        self.tail = (self.tail + 1) % self.max_size
+
+        self.full = False
+        if self.tail == self.head:
+            self.empty = True
+
+        return element
+
+    def get_capacity(self):
+        '''
+        Return buffer's capacity
+        '''
+        return self.max_size
+
+    def is_empty(self):
+        '''
+        Return true if buffer is empty else false
+        '''
+        return self.empty
+
+    def is_full(self):
+        '''
+        Return true is buffer is full else false
+        '''
+        return self.full
+
+    def get_buffer_size(self):
+        '''
+        Return current buffer occupied size
+        '''
+        if self.head >= self.tail:
+            return self.head - self.tail
+
+        return ((self.max_size - self.tail) + (self.head - 0))
 
     def clear(self):
-        pass
-
-        
+        '''
+        Clear the buffer
+        '''
+        self.buffer = [0 for i in xrange(0, self.max_size)]
+        self.head = 0
+        self.tail = 0
+        self.full = False
+        self.empty = True
+    
