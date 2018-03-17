2284fd05fccac029b7ef8e47ad0fea94fb80158b
Sri Hari Vignesh <sriharivignesh@outlook.com>
2018-03-17 00:33:46 -0700

    Clear python pyc files and __pycache__ before testing

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,8 @@
 
 pip install -U pip
 pip install -r test_requirements.txt
+
+rm -rf ring_buffer/*.pyc ring_buffer/__pycache__ test/*.pyc test/__pycache__
 pytest --cache-clear --durations=0 --color=yes --cov=ring_buffer/
+rm -rf ring_buffer/*.pyc ring_buffer/__pycache__ test/*.pyc test/__pycache__
+
